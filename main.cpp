1eb2092f06b3c99d079b8aadca9296d472f4bad9
Vladimir Kalbin <kalbin.wow@yandex.ru>
2022-02-20 17:06:00 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,15 @@
 
 int main(int argc, char *argv[]){
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
-    Game game("PingPong", 800, 600, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC, Vec2{
+    Vec2 tileSize = Vec2{
         x: 15,
         y: 100
-    }, Vec2{
+    };
+    Vec2 ballSize = Vec2{
         x: 20,
         y: 20
-    });
+    };
+    Game game("PingPong", 800, 600, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC, tileSize, ballSize);
     SDL_Quit();
     return 0;
 }
